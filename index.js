cdb663ac53d94612f1392dff841303c089cce244
Mark Rodriguez <markrodriguezaa@gmail.com>
2021-11-03 14:55:58 +0000

    complete thought process and code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //write your code here
+/*
+input a string parameter to the function
+set up an empty string called reversed to put all the individual characters in
+for loop to iterate through each character in the string starting from (string.length -1) up to an including 0 and counting down
+for each i, index into that string with i and add that character into the new string with +=
+return that string
+*/
+const reverseString = (string) => {
+    let reversedString = "";
+    for (let i = (string.length -1); i >=0; i--){
+        reversedString += string[i];
+    }
+    return reversedString;
+};
+
+console.log(reverseString("hello"))
